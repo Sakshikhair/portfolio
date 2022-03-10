cb31f96efd937029821daf0cc1d40cfe256e4099
sakshi khair <sakshikhair13@gmail.com>
2022-03-10 20:56:54 +0530

    database connected

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse 
+from home.models import Contact
 
 # Create your views here.
 def home(request):
@@ -12,4 +13,14 @@ def projects(request):
     return render(request, 'projects.html')
 
 def contact(request):
+    if request.method=="POST":
+        
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        desc = request.POST['desc']
+        print(name, email, phone, desc)
+        ins = Contact(name=name, email=email, phone=phone, desc=desc)
+        ins.save()
+        print("The data has been written to the db")
     return render(request, 'contact.html')
