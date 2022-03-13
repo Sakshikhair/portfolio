787764e2c65c08061c86fe868ca1cb09161a7c8f
sakshi <sakshikhair13@outlook.com>
2022-03-13 12:03:27 +0530

    admin page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from home import views
 
+
+admin.site.site_header = "Login"
+admin.site.site_title = "Welcome to Sakshi's Dashboard"
+admin.site.index_title = "Welcome"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
